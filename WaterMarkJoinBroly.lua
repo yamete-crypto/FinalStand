ca37877299f13494e35336a5bb3a4da2b99e3fd6
yamete-crypto <77271565+yamete-crypto@users.noreply.github.com>
2021-05-14 17:02:47 +0300

    Update WaterMarkJoinBroly.lua

--- WaterMarkJoinBroly.lua
+++ WaterMarkJoinBroly.lua
@@ -3,7 +3,7 @@
 -------------------Dont steal the script---------------------
 
 _G.AntiLeach = false --[false = so that people can enter with you and true = to go alone to all the autobots]--
-_G.GoEarth = EarthJoin --[false = teleportation to Queue and true = teleportation to Earth]--
+_G.GoEarth = false --[false = teleportation to Queue and true = teleportation to Earth]--
 _G.ChargeTime = 0 --[select here the loading time to transform, recommended max 3.9]--
 _G.FormType = "g" --[Choose between "g" and "h" depending on the transformation you like]--
 _G.ExtraTime = 2 --[[Time it will take for the script to run (recommended between 2s and 4s to avoid crashes)]]--
